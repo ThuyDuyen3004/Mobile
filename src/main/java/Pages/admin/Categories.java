466b033e2086c2a3ec06e9b33b81eccc5a9a2732
samsaysong <bobo.pro.dn@gmail.com>
2025-07-09 12:36:04 +0700

    huynp first commit

---
+++ src/main/java/Pages/admin/Categories.java
@@ -0,0 +1,29 @@
+package Pages.admin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class Categories {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private By categoriesLocator = By.xpath("//ul[@class='sidebar-menu']/li/a/span");
+
+    public Categories(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+    }
+
+    public void goToCategoryByName(String categoryName) {
+        List<WebElement> categories = driver.findElements(categoriesLocator);
+        for (WebElement category : categories) {
+            if (category.getText().trim().contentEquals(categoryName.trim())) {
+                category.click();
+                return;
+            }
+        }
+    }
+}
--- src/test/java/Test/TC_08.java
+++ src/test/java/Test/TC_08.java
@@ -1,7 +1,91 @@
 package Test;
 
 
+import Pages.admin.AddProductsPage;
+import Pages.admin.Categories;
+import Pages.admin.ViewProductPage;
+import Pages.user.HomePage;
+import Pages.user.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utils.Constants;
+
+import java.time.Duration;
+import java.util.Random;
+
 public class TC_08 {
+    @Test
+    public void testAddProductSuccessfully() throws InterruptedException {
+        // Step 1
+        driver.get(Constants.URL);
+        homePage.OpenLoginForm();
+        // Step 2
+        loginPage.login(Constants.USERNAME_ADMIN, Constants.PASSWORD_ADMIN);
+        Thread.sleep(3000);
+        // Step 3
+        driver.get(Constants.URL_ADMIN);
+        categories.goToCategoryByName("Products");
+        int idProduct = viewProductPage.getIdProduct();
+
+        // Step 4
+        viewProductPage.clickAddProductButton();
+        // Step 5 ~ Step 11
+
+        String nameProduct = new Random().nextInt(1000) + "Test Product";
+        addProductsPage.addProduct(nameProduct,
+                "100.00", "10", "5", "Apple",
+                "This is a test product description");
+        // Step 12
+
+        softAssert.assertEquals(addProductsPage.getAlertMessageSuccess(),
+                "Cập nhật thành công", "Không tìm thấy thông báo thành công"
+        );
+
+
+        categories.goToCategoryByName("Products");
+
+        int newIdProduct = viewProductPage.getIdProduct();
+        softAssert.assertTrue(newIdProduct > idProduct,
+                "Sản phẩm mới được thêm vào list Prodcut thành công");
+
+
+        softAssert.assertAll();
+    }
+
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(Constants.URL);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        softAssert = new SoftAssert();
+
+        categories = new Categories(driver, wait);
+        addProductsPage = new AddProductsPage(driver, wait);
+        viewProductPage = new ViewProductPage(driver, wait);
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+    WebDriver driver;
+    WebDriverWait wait;
+    SoftAssert softAssert;
+    Categories categories;
+    AddProductsPage addProductsPage;
+    ViewProductPage viewProductPage;
+    HomePage homePage;
+    LoginPage loginPage;
 
 
 }
